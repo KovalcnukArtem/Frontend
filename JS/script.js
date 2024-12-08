2589eca49b8939d1f20cd22d254b44f4ed0f4181
KovalcnukArtem <art.ig.kov@gmail.com>
2024-12-08 19:03:29 +0200

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,54 @@
+// console.log(12);
+
+// const myName = "Artem";
+// const age = 14;
+// const myCIty = "Lviv";
+
+// console.log(myName);
+// console.log(age);
+// console.log(myCIty);
+
+// const product = "Cup";
+// const productPrice = "2$";
+
+// console.log(product, productPrice);
+
+// const doneDistance = 50;
+// const width = "120px";
+// const notAdmin = true;
+// const isAdmin = true;
+// const isVip = true;
+// const message = "Hello user!";
+
+// alert(message);
+
+// const fruit = "Hello fruit";
+
+// alert(fruit);
+
+// const quiz = true;
+
+// const question = "Do you like sport?";
+
+// confirm(question);
+
+// const answer = confirm(question);
+// console.log(answer);
+
+// const userQuestion = "Ти хочеш продовжити підписку?";
+
+// const should = confirm(userQuestion);
+// console.log(should);
+
+// const question = "Введіть свій вік";
+// prompt(question);
+// const userQuestion = prompt(question);
+// console.log(userAge);
+
+// if (userAge >= 16) {
+//     alert("Ви дорослі")
+// }
+
+// if (userAge < 16) {
+//   alert("Ви дитина");
+// }
