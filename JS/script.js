ed30a8f0bdfe4e8a3a9a5e7bbd96a4416ee59492
KovalcnukArtem <art.ig.kov@gmail.com>
2025-01-19 16:27:31 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -522,3 +522,166 @@
 //   }
 // }
 // console.log(message);
+
+// 1
+// 1.перетвори масив у рядок, без роздільників,
+// 2.поверни кількість символів в отриманому рядку.
+
+// ["Mango", "hurries", "to", "the", "train"] повертає 22
+// ["M", "a", "n", "g", "o"] повертає 5
+// ["top", "picks", "for", "you"] повертає 14
+
+// const array = ["Mango", "hurries", "to", "the", "train"];
+// const convertedArray = array.join("");
+// console.log(convertedArray);
+
+// const length = convertedArray.length
+// console.log(length);
+
+// const storage = ["apple", "plum", "pear"];
+// const item = "peAr";
+
+// const convertedItem = item.toLowerCase();
+// console.log(convertedItem);
+
+// const isIncluded = storage.includes(convertedItem);
+// console.log(isIncluded);
+// let answer;
+
+// if (isIncluded) {
+//     console.log(`${item} is available to order!`);
+//     answer = `${item} is available to order!`
+// } else {
+//     console.log("Sorry! We are out of stock!");
+//     answer = "Sorry! We are out of stock!"
+// }
+// практика шарити екран
+//  4
+// Створити масив із п'яти чисел. Використовуючи метод indexOf(),
+// знайти індекс елемента зі значенням 3.
+
+// /  4
+// Створити масив із п'яти чисел. Використовуючи метод indexOf(),
+// знайти індекс елемента зі значенням 3.
+
+// const numbers = [0, 1, "jkljl;kjkl", 3, 4];
+
+// console.log(numbers.indexOf(3));
+
+// 5
+// Створити масив із п'яти стрічок. Використовуючи метод join(),
+//  створити одну стрічку, яка буде містити всі елементи масиву,
+//  розділені комою.
+
+// const strings = ["q", "w", "e", "r", "t"];
+
+// console.log(strings.join(","));
+
+//                                                      lesson 8
+// 1
+
+// min - ціле число, з якого починаються обчислення
+// max - ціле число, до якого включно триватимуть обчислення
+// Доповни код таким чином, щоб повертати масив усіх цілих чисел
+//  від значення min до max включно.
+
+// (1, 3) повертає [1, 2, 3]
+// (14, 17) повертає [14, 15, 16, 17]
+// В циклі for використовуй метод push
+
+// const min = 10;
+// const max = 15;
+
+// let array = [];
+
+// for(let i = min; i <= max; i += 1) {
+//     console.log(i);
+//     array.push(i)
+// }
+
+// console.log(array);
+// 2
+
+// Знайти спільні елементи.
+// Спільними елементами  - які присутні у всіх масивах.
+// Наприклад,
+// у двох масивах [1, 3, 5] і [0, 8, 5, 3] спільними будуть числа 3 і 5,
+
+// 1. створи змінні array1 та array2
+// 2. Створи порожній масив для зберігання нового масиву.
+// 3. Використай цикл for для ітерації кожного елемента у array1.
+// У тілі циклу перевір, чи поточний елемент існує у array2
+// за допомогою методу includes.
+// Якщо він існує, то додай елемент до нового масиву.
+// [1, 2,
+// const array1 = [24, 12, 27, 3];
+// const array2 = [12, 8, 3, 36, 27];
+
+// const newArray = [];
+
+// for( let i = 0; i < array1.length; i += 1) {
+//     // console.log(array1[i]);
+//     // console.log(array2.includes(array1[i]));
+//     if (array2.includes(array1[i])) {
+//         newArray.push(array1[i])
+//     }
+// }
+
+// console.log(newArray);
+
+// 3
+// Створити масив із п'яти чисел. Використовуючи метод push(),
+//  додати до масиву ще один елемент.
+
+// const numbers = [0, 1, 2, 3, 4];
+
+// numbers.push(5);
+
+// // console.log(numbers.push(5));
+// console.log(numbers);
+
+// 4
+// Створити масив із трьох стрічок. Використовуючи метод pop(),
+// видалити з масиву останній елемент.
+// const strings = ["string1", "strin2", "strin3"];
+
+// strings.pop();
+
+// console.log(strings);
+
+// Доповни код таким чином,
+// щоб змінні містили часткові копії вихідного масиву fruits.
+
+// firstTwoEls - масив із перших двох елементів
+// nonExtremeEls - масив з усіх елементів, крім першого та останнього
+// lastThreeEls - масив із трьох останніх елементів
+
+// const fruits = ["apple", "plum", "pear", "orange", "banana"];
+
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(-3);
+
+// // console.log(firstTwoEls);
+// // console.log(nonExtremeEls);
+// // console.log(lastThreeEls);
+
+// const array = ["Mango", "Poly", "Ajax"];
+// const value = "Poly";
+// let arr = [];
+
+// const index = array.indexOf(value);
+
+// // console.log(arr);
+
+// if (index !== -1) {
+//   arr = array.slice(0, index + 1);
+// }
+
+// 4
+// Створити масив із десяти чисел. Використовуючи метод slice(),
+//  створити новий масив, який буде містити перші п'ять елементів.
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const array = arr.slice(0, 6);
+
+// console.log(array);
